97088e7b9a3dddd3b9c0a9d1338a73b2a7a4cbdd
MG <emerenmg@gmail.com>
2019-09-15 23:11:05 +0200

    Updates

---
+++ precache-manifest.bad57ac7fef09040c2f3342f598996f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "357f974552491da1b1cd8cf89dfa1ac6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d8784160ce3594f0a31",
+    "url": "/static/css/main.7990008b.chunk.css"
+  },
+  {
+    "revision": "25fc9bf58f95c6fbb645",
+    "url": "/static/js/2.2c2b65b4.chunk.js"
+  },
+  {
+    "revision": "7d8784160ce3594f0a31",
+    "url": "/static/js/main.8bb8b775.chunk.js"
+  },
+  {
+    "revision": "c49018507a564c0ada45",
+    "url": "/static/js/runtime~main.b908f969.js"
+  },
+  {
+    "revision": "e697440f12ae5610921a4368a147ef0d",
+    "url": "/static/media/cc.e697440f.png"
+  },
+  {
+    "revision": "0ce1c82dfac29ce8c07cbc44fbe687db",
+    "url": "/static/media/dark-roz3.0ce1c82d.jpg"
+  },
+  {
+    "revision": "b3fb53a63a63afe05634b8e4ac0faf50",
+    "url": "/static/media/fda.b3fb53a6.png"
+  },
+  {
+    "revision": "2e21dd3ec36e63f6a33070099a73b557",
+    "url": "/static/media/rtbg.2e21dd3e.png"
+  }
+]);
